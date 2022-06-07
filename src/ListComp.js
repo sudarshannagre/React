b9d0d47656bb5c014c62f6b6ba28ce62ed49cebf
sudarshan nagre <sudarshan.nagre@vodafone.com>
2022-06-07 09:31:55 +0530

    7June

--- src/ListComp.js
+++ src/ListComp.js
@@ -1,33 +1,72 @@
 import axios from "axios";
-import React from "react";
+import React, { useEffect, useState } from "react";
 import { useNavigate } from "react-router-dom";
 import Navbar from "./Navbar";
+import Table from 'react-bootstrap/Table'
 
 function ListComp(){
     const navigate=useNavigate();
+    const [studentList, setStudentList]=useState([]);
 
-    const name  = localStorage.getItem("userName").split(":")[1];
-
-    function list(){
-        alert("list");
-
-        axios.get("http://localhost:8080/v1/student").then(response =>{
+    const deleteStudent = (event, source) => {
+        console.log("Id : "+source);
+        axios.delete("http://localhost:8080/v1/student?id="+source).then(response=>{
             alert(response.data);
-
-
-        }).catch(error =>{
-            alert("something went wrong !!"+error);
+            window.location.reload()
+        }).catch(errore =>{
+            alert(errore);
         });
+    };
 
+   async function list(){
+
+        fetch("http://localhost:8080/v1/student")
+        .then(res => res.json())
+        .then(json => setStudentList(json));
 
     }
 
+    React.useEffect(() => {
+        list();
+    }, []);
+
+    
+
     return(
         <>
-        <Navbar/>
-       
-        <button onClick={list}>List of Employee</button>
+      
+      <Table striped bordered hover>
+                <thead>
+                    <tr>
+                        <th>Name</th>
+                        <th>Mobile</th>
+                        <th>Email</th>
+                        <th>Address</th>
+                        <th>Technology</th>
+                        <th>BatchName</th>
+                        <th>JoiningDate</th>
+                        <th>Operation</th>
+                    </tr>
+                </thead>
+                <tbody>
+                    {
+                    studentList.map((student, index) => (
+                        <tr key={index}>
+                            <td>{student.name}</td>
+                            <td>{student.mobile}</td>
+                            <td>{student.email}</td>
+                            <td>{student.address}</td>
+                            <td>{student.technology}</td>
+                            <td>{student.bacthName}</td>
+                            <td>{student.joiningDate}</td>
+                            <td><button onClick={(event)=>deleteStudent(event,student.id)}>Delete</button></td>
+                        </tr>
+                    ))
+                    }
 
+                </tbody>
+
+            </Table>
        </>
     );
 
--- src/Navbar.js
+++ src/Navbar.js
@@ -3,25 +3,25 @@ import React from "react";
 function Navbar() {
     const name  = localStorage.getItem("userName").split(":")[1];
     return (
-        <nav class="navbar navbar-expand-lg navbar-light bg-light">
-            <div class="container-fluid">
-                <a class="navbar-brand" href="#">Navbar w/ text</a>
-                <button class="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarText" aria-controls="navbarText" aria-expanded="false" aria-label="Toggle navigation">
-                    <span class="navbar-toggler-icon"></span>
+        <nav className="navbar navbar-expand-lg navbar-light bg-light">
+            <div className="container-fluid">
+                <a className="navbar-brand" href="#">Navbar w/ text</a>
+                <button className="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarText" aria-controls="navbarText" aria-expanded="false" aria-label="Toggle navigation">
+                    <span className="navbar-toggler-icon"></span>
                 </button>
-                <div class="collapse navbar-collapse" id="navbarText">
-                    <ul class="navbar-nav me-auto mb-2 mb-lg-0">
-                        <li class="nav-item">
-                            <a class="nav-link active" aria-current="page" href="/">Home</a>
+                <div className="collapse navbar-collapse" id="navbarText">
+                    <ul className="navbar-nav me-auto mb-2 mb-lg-0">
+                        <li className="nav-item">
+                            <a className="nav-link active" aria-current="page" href="/">Home</a>
                         </li>
-                        <li class="nav-item">
-                            <a class="nav-link" href="list">ListComp</a>
+                        <li className="nav-item">
+                            <a className="nav-link" href="list">ListComp</a>
                         </li>
-                        <li class="nav-item">
-                            <a class="nav-link" href="login">LoginClass</a>
+                        <li className="nav-item">
+                            <a className="nav-link" href="login">LoginName</a>
                         </li>
                     </ul>
-                    <span class="navbar-text">
+                    <span className="navbar-text">
                         Welcome, {name}
                     </span>
                 </div>
